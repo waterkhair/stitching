ca19494c94e9638501c55fbce1284cdcb1c80ad7
WaterKhair <waterkahir@gmail.com>
2017-11-27 23:29:35 -0800

    Adding missing index.js file.

---
+++ index.js
@@ -0,0 +1,9 @@
+// Modules
+const stitching = require("./src/index");
+
+module.exports = {
+    auth: stitching.auth,
+    client: stitching.client,
+    connect: stitching.connect,
+    db: stitching.db
+};
